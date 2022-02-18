985c369fcb40dd7ea85b44ac0fb89e7dc71e290c
jseop-lim <jseoplim@gmail.com>
2022-02-18 16:47:06 +0900

    Chore(rental): Remove unnecessary methods
    student views에서 주석처리 되었던 write 관련 메서드 아예 지워버림
    StudentListView에서 post method 삭제
    StudentDetailView에서 put, delete method 삭제

--- rental/views.py
+++ rental/views.py
@@ -16,17 +16,10 @@ class StudentListView(APIView):
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data)
 
-    # def post(self, request, format=None):
-    #     serializer = StudentSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 class StudentDetailView(APIView):
     """
-    Retrieve or update a student.
+    Retrieve a student.
     """
     def get_object(self, pk):
         return get_object_or_404(Student, pk=pk)
@@ -35,16 +28,3 @@ class StudentDetailView(APIView):
         student = self.get_object(pk)
         serializer = StudentSerializer(student)
         return Response(serializer.data)
-
-    # def put(self, request, pk, format=None):
-    #     student = self.get_object(pk)
-    #     serializer = StudentSerializer(student, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # def delete(self, request, pk, format=None):
-    #     student = self.get_object(pk)
-    #     student.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
