c3e2f3035add9d8c9fc39715d071ee4f46af611e
jseop-lim <jseoplim@gmail.com>
2022-02-19 21:58:09 +0900

    Test(rental): Define PurchaseViewTest and tests
    * TestCase class 내에 setUpClass 메서드 정의하여 test마다 setup에서 생성한
      ticket 객체를 그대로 유지(id 그대로)
    * Make test for purchase-create, in api and django ORM.
    * shell과 api를 통한 purchase-create를 따로 테스트하여
      perform_create()와 receiver 작동의 차이를 확인. (#16)

--- rental/tests.py
+++ rental/tests.py
@@ -1,3 +1,66 @@
-from django.test import TestCase
+import json
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rental.models import *
 
-# Create your tests here.
+    
+class PurchaseViewTest(APITestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.tickets = {}
+        cls.tickets[1] = Ticket.objects.create(time=10, storable=False, price=1000)
+        cls.tickets[2] = Ticket.objects.create(time=5, storable=True, price=1500)
+    
+    
+    def create_user(self, login=False, username='user', password='1234', email="user@test.com", name='Kim'):
+        user = User.objects.create_user(username, email, password)
+        student = Student.objects.create(user=user, name=name)
+        if login:
+            self.assertTrue(self.client.login(username=username, password=password))
+        return user
+
+    
+    def test_purchase_create_api(self):
+        """
+        purchase-list에서 POST 요청으로 purchase 생성
+        """
+        student = self.create_user(login=True).student
+        residual_time = student.residual_time
+        ticket = self.tickets[2]  # 0:00:05 / 1,500원 (storable)
+        
+        data = {"ticket": ticket.id}
+        response = self.client.post(reverse('rental:purchase-list'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # purchase 필드 일치 확인
+        purchase = Purchase.objects.get(pk=json.loads(response.content)['id'])
+        self.assertEqual(purchase.student, student)
+        self.assertEqual(purchase.ticket, ticket)
+        
+        # student 필드 변경 확인
+        student.refresh_from_db()
+        self.assertEqual(student.residual_time, residual_time + ticket.time)
+        self.assertTrue(student.storable == ticket.storable == True)  # False -> True로 바뀜
+        
+    
+    def test_purchase_create_shell(self):
+        """
+        Django ORM을 이용한 purchase 생성
+        (model receiver 적용 시 student 필드 값 변경)
+        """
+        student = self.create_user(login=True).student
+        residual_time = student.residual_time
+        ticket = self.tickets[2]  # 0:00:05 / 1,500원 (storable)
+        
+        # purchase 필드 일치 확인
+        purchase = Purchase.objects.create(student=student, ticket=ticket)
+        self.assertEqual(purchase.student, student)
+        self.assertEqual(purchase.ticket, ticket)
+        
+        # student 필드 변경 확인
+        student.refresh_from_db()
+        self.assertEqual(student.residual_time, residual_time + ticket.time)
+        self.assertEqual(student.storable, ticket.storable)  # False -> True로 바뀌지 않음
+        
