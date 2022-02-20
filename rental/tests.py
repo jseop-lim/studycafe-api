e3b8222c68b4cf5c9d1e5d1d3241fdee65a5f468
jseop-lim <jseoplim@gmail.com>
2022-02-20 22:10:48 +0900

    Test(rental): student-purchase

--- rental/tests.py
+++ rental/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rental.models import *
+from rental.serializers import PurchaseStudentSerializer
 
     
 class PurchaseViewTest(APITestCase):
@@ -63,4 +64,42 @@ class PurchaseViewTest(APITestCase):
         student.refresh_from_db()
         self.assertEqual(student.residual_time, residual_time + ticket.time)
         self.assertEqual(student.storable, ticket.storable)  # False -> True로 바뀌지 않음
+    
+
+class StudentViewTest(APITestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.tickets = {}
+        cls.tickets[1] = Ticket.objects.create(time=10, storable=False, price=1000)
+        cls.tickets[2] = Ticket.objects.create(time=5, storable=True, price=1500)
+    
+    
+    def create_user(self, login=False, username='user', password='1234', email="user@test.com", name='Kim'):
+        user = User.objects.create_user(username, email, password)
+        student = Student.objects.create(user=user, name=name)
+        if login:
+            self.assertTrue(self.client.login(username=username, password=password))
+        return user
+    
+
+    def test_student_purchase(self):
+        """
+        student-purchase에서 학생과 연결된 purchases가 잘 나오는지 확인
+        """
+        std1 = self.create_user(username='user1', name='Kim').student
+        Purchase.objects.create(student=std1, ticket=self.tickets[1])
+        Purchase.objects.create(student=std1, ticket=self.tickets[1])
+        Purchase.objects.create(student=std1, ticket=self.tickets[2])
+        
+        std2 = self.create_user(username='user2', name='Lee').student
+        Purchase.objects.create(student=std2, ticket=self.tickets[1])
+        
+        std1.refresh_from_db()
+        serializer = PurchaseStudentSerializer(std1.purchases, many=True)
+        
+        response = self.client.get(reverse('rental:student-purchase', args=[std1.user_id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertJSONEqual(response.content, serializer.data)
+        
         
