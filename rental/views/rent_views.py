db99a8a7a70d25590a179bf030c13209e554210d
jseop-lim <jseoplim@gmail.com>
2022-02-27 11:35:51 +0900

    Chore(rental): Optimize import modules in rent_views.py

--- rental/views/rent_views.py
+++ rental/views/rent_views.py
@@ -1,11 +1,7 @@
-from django.utils import timezone
-
 from rental.models import Seat, Rent
 from rental.serializers import SeatSerializer, RentSerializer
 
 from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.exceptions import ValidationError
 
 
